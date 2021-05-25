f311e388d0585b7e789847c4a5bad79e8fb26b25
ltrel <79512656+ltrel@users.noreply.github.com>
2021-05-26 09:52:12 +1000

    Run linting on backend skeleton code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
-const port = 21487
-const app = express()
+const port = 21487;
+const app = express();
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 });
