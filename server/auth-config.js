3e6290da1d9052bed7c5705c30cde2e3bdff3506
ltrel <79512656+ltrel@users.noreply.github.com>
2021-05-30 17:57:10 +1000

    Create separate verifyLogin function

--- server/auth-config.js
+++ server/auth-config.js
@@ -1,42 +1,40 @@
 const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
 
-/**
- * @param {Passport} passport - Passport module.
- * @param {Sequelize} sequelize - Database connection.
- */
 function initialize(passport, sequelize) {
+  async function verifyLogin(username, password, done) {
+    try {
+      // Search for the user in the database
+      const user = await sequelize.models.User.findOne({
+        where: {
+          username: username,
+        },
+      });
+      // Make sure the user exists.
+      if (!user) {
+        return done(null, false, {message: 'User not found'});
+      }
+
+      // Check the password.
+      const validPass = await bcrypt.compare(password, user.password);
+      if (!validPass) {
+        return done(null, false, {message: 'Password incorrect'});
+      }
+
+      return done(null, user, {message: 'Success'});
+    } catch (e) {
+      // Something went wrong.
+      return done(e);
+    }
+  }
+
   passport.use('login',
       new LocalStrategy(
           {
             usernameField: 'username',
             passwordField: 'password',
           },
-          async (username, password, done) => {
-            try {
-              // Search for the user in the database
-              const user = await sequelize.models.User.findOne({
-                where: {
-                  username: username,
-                },
-              });
-              // Make sure the user exists.
-              if (!user) {
-                return done(null, false, {message: 'User not found'});
-              }
-
-              // Check the password.
-              const validPass = await bcrypt.compare(password, user.password);
-              if (!validPass) {
-                return done(null, false, {message: 'Password incorrect'});
-              }
-
-              return done(null, user, {message: 'Success'});
-            } catch (e) {
-              // Something went wrong.
-              return done(e);
-            }
-          },
+          verifyLogin,
       ),
   );
 }
