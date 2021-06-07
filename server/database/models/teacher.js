fcad1597e417ed4bb32694f0a4bddf1955be4cb5
ltrel <79512656+ltrel@users.noreply.github.com>
2021-06-07 14:43:55 +1000

    Rename StudentTeachers to TeacherStudents

--- server/database/models/teacher.js
+++ server/database/models/teacher.js
@@ -6,6 +6,6 @@ module.exports.addAssociations = ({models}) => {
   models.Teacher.hasMany(models.Location);
   models.Location.belongsTo(models.Teacher);
 
-  models.Teacher.belongsToMany(models.Student, {through: 'StudentTeachers'});
-  models.Student.belongsToMany(models.Teacher, {through: 'StudentTeachers'});
+  models.Teacher.belongsToMany(models.Student, {through: 'TeacherStudents'});
+  models.Student.belongsToMany(models.Teacher, {through: 'TeacherStudents'});
 };
