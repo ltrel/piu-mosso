1bfc28e67c3be13b4198e208ed9cce85cee5ae50
ltrel <79512656+ltrel@users.noreply.github.com>
2021-06-17 12:06:21 +1000

    Clean up authorization code for POST /lessons

--- server/routes/lessons.js
+++ server/routes/lessons.js
@@ -19,13 +19,6 @@ function initialize(sequelize) {
       return res.status(400).json({errors: errors.array});
     }
 
-    // Find teacher with id from request.
-    const user = await sequelize.models.User.findOne({
-      where: {id: req.user.id}});
-    const teacher = await user.getTeacher();
-    // Respond with authentication error if that user isn't a teacher.
-    if (teacher === null) return res.sendStatus(401);
-
     // Find student specified in request.
     const student = await sequelize.models.Student.findOne({
       where: {id: req.body.studentId}});
@@ -37,11 +30,11 @@ function initialize(sequelize) {
     if (instrument === null) return res.sendStatus(400);
 
     // Create the lesson.
-    const lesson = await teacher.createLesson({
+    const lesson = await req.teacher.createLesson({
       dateTime: req.body.dateTime,
       minutes: req.body.minutes,
     });
-    lesson.setTeacher(teacher);
+    lesson.setTeacher(req.teacher);
     lesson.setStudent(student);
     lesson.setInstrument(instrument);
 
