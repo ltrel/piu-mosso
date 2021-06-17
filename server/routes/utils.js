2d0128628bec34a7c2845db7894d1fc30a6a24da
ltrel <79512656+ltrel@users.noreply.github.com>
2021-06-17 11:52:56 +1000

    Expand authorizeUserType to cover students

--- server/routes/utils.js
+++ server/routes/utils.js
@@ -1,4 +1,5 @@
 function authorizeUserType(type, sequelize) {
+  // Return the relevant function depending on the type parameter.
   if (type === 'teacher') {
     return (async (req, res, next) => {
       // Find teacher with id from request
@@ -11,14 +12,14 @@ function authorizeUserType(type, sequelize) {
       // Continue to the next middleware function.
       next();
     });
-  } else {
+  } else if (type === 'student') {
     return (async (req, res, next) => {
-      // Find teacher with id from request
+      // Find student with id from request
       const user = await sequelize.models.User.findOne({
         where: {id: req.user.id}});
-      req.teacher = await user.getTeacher();
-      // Respond with authentication error if there is no teacher with that id.
-      if (req.teacher === null) return res.sendStatus(401);
+      req.student = await user.getStudent();
+      // Respond with authentication error if there is no student with that id.
+      if (req.student === null) return res.sendStatus(401);
 
       // Continue to the next middleware function.
       next();
