48f5d5daaa577f00ad9ff2f7015abe721cc628f2
ltrel <79512656+ltrel@users.noreply.github.com>
2021-07-03 13:39:08 +1000

    Fix unintentional nesting of file download tests

--- server/test/files.test.js
+++ server/test/files.test.js
@@ -96,75 +96,75 @@ describe('Shared Files', function() {
           .expect(401);
       assert.deepStrictEqual(res.body, {});
     });
+  });
 
-    describe('GET /files/download', function() {
-      it('Downloads files from the server', async function() {
-        // Copy a file to the uploads directory.
-        fs.copyFileSync(
-            path.join(__dirname, 'bach.pdf'),
-            path.join(uploadsDir, 'bach.pdf'));
-        // Add database entry for the file.
-        const fileEntry = await sequelize.models.File.create({
-          fileName: 'bach.pdf',
-          filePath: path.join(uploadsDir, 'bach.pdf'),
-          dateTime: Date.now(),
-        });
-        fileEntry.setTeacher(teacher);
-        fileEntry.setStudent(await studentUsers[0].getStudent());
+  describe('GET /files/download', function() {
+    it('Downloads files from the server', async function() {
+      // Copy a file to the uploads directory.
+      fs.copyFileSync(
+          path.join(__dirname, 'bach.pdf'),
+          path.join(uploadsDir, 'bach.pdf'));
+      // Add database entry for the file.
+      const fileEntry = await sequelize.models.File.create({
+        fileName: 'bach.pdf',
+        filePath: path.join(uploadsDir, 'bach.pdf'),
+        dateTime: Date.now(),
+      });
+      fileEntry.setTeacher(teacher);
+      fileEntry.setStudent(await studentUsers[0].getStudent());
 
-        // Request to download the file.
-        const teacherRes = await request(await server)
-            .get('/files/download')
-            .query({auth_token: token})
-            .send({fileId: fileEntry.id})
-            .expect(200);
-        const studentRes = await request(await server)
-            .get('/files/download')
-            .query({auth_token: token})
-            .send({fileId: fileEntry.id})
-            .expect(200);
+      // Request to download the file.
+      const teacherRes = await request(await server)
+          .get('/files/download')
+          .query({auth_token: token})
+          .send({fileId: fileEntry.id})
+          .expect(200);
+      const studentRes = await request(await server)
+          .get('/files/download')
+          .query({auth_token: token})
+          .send({fileId: fileEntry.id})
+          .expect(200);
 
-        // Compare the responses with the original file.
-        const originalBuffer = fs.readFileSync(
-            path.join(__dirname, 'bach.pdf'));
-        assert(teacherRes.body.equals(originalBuffer));
-        assert(studentRes.body.equals(originalBuffer));
+      // Compare the responses with the original file.
+      const originalBuffer = fs.readFileSync(
+          path.join(__dirname, 'bach.pdf'));
+      assert(teacherRes.body.equals(originalBuffer));
+      assert(studentRes.body.equals(originalBuffer));
+    });
+    it('Rejects requests from users without access', async function() {
+      // Copy a file to the uploads directory.
+      fs.copyFileSync(
+          path.join(__dirname, 'bach.pdf'),
+          path.join(uploadsDir, 'bach.pdf'));
+      // Add database entry for the file.
+      const fileEntry = await sequelize.models.File.create({
+        fileName: 'bach.pdf',
+        filePath: path.join(uploadsDir, 'bach.pdf'),
+        dateTime: Date.now(),
       });
-      it('Rejects requests from users without access', async function() {
-        // Copy a file to the uploads directory.
-        fs.copyFileSync(
-            path.join(__dirname, 'bach.pdf'),
-            path.join(uploadsDir, 'bach.pdf'));
-        // Add database entry for the file.
-        const fileEntry = await sequelize.models.File.create({
-          fileName: 'bach.pdf',
-          filePath: path.join(uploadsDir, 'bach.pdf'),
-          dateTime: Date.now(),
-        });
-        fileEntry.setTeacher(teacher);
-        fileEntry.setStudent(await studentUsers[1].getStudent());
+      fileEntry.setTeacher(teacher);
+      fileEntry.setStudent(await studentUsers[1].getStudent());
 
-        const res = await request(await server)
-            .get('/files/download')
-            .query({auth_token: studentToken})
-            .send({fileId: fileEntry.id})
-            .expect(401);
-        assert.deepStrictEqual(res.body, {});
-      });
-      it('Requires requests to include a fileId', async function() {
-        await request(await server)
-            .get('/files/download')
-            .query({auth_token: token})
-            .expect(400);
-      });
-      it('Rejects nonexistent fileIds', async function() {
-        const res = await request(await server)
-            .get('/files/download')
-            .query({auth_token: token})
-            .send({fileId: 0})
-            .expect(400);
-        assert.deepStrictEqual(res.body, {});
-      });
+      const res = await request(await server)
+          .get('/files/download')
+          .query({auth_token: studentToken})
+          .send({fileId: fileEntry.id})
+          .expect(401);
+      assert.deepStrictEqual(res.body, {});
+    });
+    it('Requires requests to include a fileId', async function() {
+      await request(await server)
+          .get('/files/download')
+          .query({auth_token: token})
+          .expect(400);
+    });
+    it('Rejects nonexistent fileIds', async function() {
+      const res = await request(await server)
+          .get('/files/download')
+          .query({auth_token: token})
+          .send({fileId: 0})
+          .expect(400);
+      assert.deepStrictEqual(res.body, {});
     });
   });
 
