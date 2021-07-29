5404882c6c20cfd60674df5ac46469a6fbc86952
ltrel <79512656+ltrel@users.noreply.github.com>
2021-07-29 15:17:49 +1000

    Add DELETE method to /teacher-students

--- server/routes/teacher-students.js
+++ server/routes/teacher-students.js
@@ -24,6 +24,23 @@ function initialize(sequelize) {
     return res.sendStatus(200);
   });
 
+  // Route for deleting students
+  router.delete('/', body('studentId').isInt(), async (req, res) => {
+    // Return validation errors if any were found.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array});
+    }
+
+    // Find student specified in request.
+    const student = await sequelize.models.Student.findOne({
+      where: {id: req.body.studentId}});
+    if (student === null) return res.sendStatus(400);
+
+    await req.teacher.removeStudent(student);
+    return res.sendStatus(200);
+  });
+
   router.get('/', async (req, res) => {
     // Find all the teacher's students.
     const students = await req.teacher.getStudents();
